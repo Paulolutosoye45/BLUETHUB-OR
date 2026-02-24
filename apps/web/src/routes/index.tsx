57932b382f654594a9748909f00fc4086d45832a
paul olutosoye <paulolutosoye@gmail.com>
2026-02-24 18:49:37 +0100

    feat: added reply-2

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -4,6 +4,8 @@ import NotFound from '@/component/not-found';
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import ClassRoom from '@/layouts/teacher/class/class-room';
 import Replay from '@/component/reply';
+import { Provider } from 'react-redux';
+import { store } from '@/store';
 
 const router = createBrowserRouter([
     {
@@ -16,7 +18,7 @@ const router = createBrowserRouter([
     },
     {
         path: '/replay',
-        element: <Replay />
+        element: <Provider store={store}><Replay /> </Provider>
     },
     {
         path: "*",
